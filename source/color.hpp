e1f28be0f4549b1a374bd0e4905caccc1a3e4f39
Nicola20 <nicola.lea.libera@uni-weimar.de>
2018-04-30 21:57:53 +0200

    Corrected version of color

--- source/color.hpp
+++ source/color.hpp
@@ -1,9 +1,15 @@
+# ifndef COLOR_HPP
+# define COLOR_HPP
+
 struct Color
 {
+    public:
+    Color (float r, float g, float b);
+    Color (float f);
+
+    //private:    
     float r_;
     float g_;
     float b_;
-
-    Color (float r, float g, float b);
-    Color (float f);
 };
+# endif // COLOR_HPP
